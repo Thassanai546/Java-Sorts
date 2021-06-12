e90658d60f9e82965808b83341fe552ebc31d925
Thass McCabe <thassanaimccabe@gmail.com>
2021-06-12 20:14:43 +0100

    Added partition method for QS

--- Sorts.java
+++ Sorts.java
@@ -1,10 +1,34 @@
 public class Sorts {
 
     public static void swap(int[] arr,int a,int b){
-        //Swap two elements in an array
+        // Swap two elements in an array
         int temp = arr[a];
         arr[a] = arr[b];
         arr[b] = temp;
+    }
+
+    public static int partition(int[] arr,int l, int r){
+        // used in quick sort
+        int pivot = arr[r];
+        int i = l - 1;
+        for(int j=l;j<r;j++){
+            if(arr[j] < pivot){
+                i+=1;
+                swap(arr, i, j);
+            }
+        }
+        // Move pivot
+        i++;
+        swap(arr, i, r);
+        return i + 1;
+    }
+
+    public static void quickSort(int[] arr, int l, int r){
+        if (l >= r) return;
+        int p = partition(arr, l, r);
+
+        quickSort(arr, l, p-1);
+        quickSort(arr, p+1, r);
     }
 
     public static void selectionSort(int[] arr){
@@ -23,7 +47,7 @@ public class Sorts {
 
     public static void bubbleSort(int[] arr){
         for(int j=0;j<arr.length;j++){ //Repeat until no swaps needed
-            for(int i=0;i<arr.length-1;i++){
+            for(int i=0;i<arr.length;i++){
                 if(arr[i+1] < arr[i]){ //If neighbouring elements out of order, swap them
                     swap(arr, i+1, i);
                 }
@@ -32,13 +56,13 @@ public class Sorts {
     }
 
     public static void main(String[] args) {
-        int[] arr = {64, 25, 12, 22, 11};
+        int[] arr = {64, 25, 12, 22, 11, 30};
 
         System.out.println("Initial array: ");
         for(int x:arr) System.out.print(x + " ");
 
         System.out.println();
-        selectionSort(arr);
+        partition(arr, 0, 5);
 
         System.out.println("Resulting array: ");
         for(int x:arr) System.out.print(x + " ");

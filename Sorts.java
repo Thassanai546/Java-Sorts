eadb9d174f4db5bf11c5b656e7656ed5ef0a2ff5
Thass McCabe <thassanaimccabe@gmail.com>
2021-06-12 13:09:31 +0100

    Changed name, added bubble sort

--- Sorts.java
+++ Sorts.java
@@ -1,19 +1,33 @@
-public class selectionSort {
+public class Sorts {
+
+    public static void swap(int[] arr,int a,int b){
+        //Swap two elements in an array
+        int temp = arr[a];
+        arr[a] = arr[b];
+        arr[b] = temp;
+    }
 
     public static void selectionSort(int[] arr){
-
         int indexMin=0;
-
         for(int i=0;i<arr.length;i++){
+            //Find index of smallest entry in arr[i...n-1]
             indexMin = i;
             for(int j=i+1;j< arr.length;j++){
                 if(arr[j] < arr[indexMin]){
                     indexMin = j;
                 }
             }
-            int temp = arr[i];
-            arr[i] = arr[indexMin];
-            arr[indexMin] = temp;
+            swap(arr,i,indexMin);
+        }
+    }
+
+    public static void bubbleSort(int[] arr){
+        for(int j=0;j<arr.length;j++){ //Repeat until no swaps needed
+            for(int i=0;i<arr.length-1;i++){
+                if(arr[i+1] < arr[i]){ //If neighbouring elements out of order, swap them
+                    swap(arr, i+1, i);
+                }
+            }
         }
     }
 
